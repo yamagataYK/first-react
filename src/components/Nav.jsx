1e015fda5f2898f54b1a8a8546f2255be3117d44
Yamagata Kaito <168725251+yamagataYK@users.noreply.github.com>
2024-11-08 11:56:19 +0900

    ファイルの編集

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Nav() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href=""></a>home</li>
+                    <li><a href=""></a>about</li>
+                    <li><a href=""></a>works</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "../components/nav"
+import "../style/pages/App.css"
+
+export default function App() {
+
+
+  return (
+    <div>
+      <Nav />
+      <h1>hello</h1>
+    </div>
+  )
+}
